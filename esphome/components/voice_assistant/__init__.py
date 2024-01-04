fbaa3970e552081d8d18ccaf14e8dc20aa1bdae1
Hendrik Langer <hendrik+dev@xd0.de>
2024-01-04 22:39:11 +0100

    target

--- esphome/components/voice_assistant/__init__.py
+++ esphome/components/voice_assistant/__init__.py
@@ -277,6 +277,7 @@ async def to_code(config):
     cg.add_build_flag("-DTF_LITE_STATIC_MEMORY")
     cg.add_build_flag("-DTF_LITE_DISABLE_X86_NEON")
     cg.add_build_flag("-DESP_NN")
+    cg.add_build_flag("-DCONFIG_IDF_TARGET_ESP32")
 
 
 VOICE_ASSISTANT_ACTION_SCHEMA = cv.Schema({cv.GenerateID(): cv.use_id(VoiceAssistant)})
