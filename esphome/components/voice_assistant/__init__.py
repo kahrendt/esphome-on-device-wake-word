a9b09daf705fd35dcd7f7b487b3f8bb630bcc57f
Hendrik Langer <hendrik+dev@xd0.de>
2024-01-04 22:48:32 +0100

    remove esp_nn

--- esphome/components/voice_assistant/__init__.py
+++ esphome/components/voice_assistant/__init__.py
@@ -276,8 +276,9 @@ async def to_code(config):
 
     cg.add_build_flag("-DTF_LITE_STATIC_MEMORY")
     cg.add_build_flag("-DTF_LITE_DISABLE_X86_NEON")
-    cg.add_build_flag("-DESP_NN")
+    #cg.add_build_flag("-DESP_NN")
     cg.add_build_flag("-DCONFIG_IDF_TARGET_ESP32")
+    # CONFIG_IDF_TARGET_ESP32S3
 
 
 VOICE_ASSISTANT_ACTION_SCHEMA = cv.Schema({cv.GenerateID(): cv.use_id(VoiceAssistant)})
